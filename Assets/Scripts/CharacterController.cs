34d35a236f9c31bd97dd6301226b745ae319d935
Mark <markrassamni@me.com>
2017-02-24 15:29:52 -0800

    joystick

---
+++ Assets/Scripts/CharacterController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class CharacterController : MonoBehaviour {
+
+	[SerializeField] private float moveSpeed;
+	[SerializeField] private float jumpHeight;
+
+	private Rigidbody rigidBody;
+	private Animator anim;
+	void Start () {
+		rigidBody = GetComponent<Rigidbody>();
+		anim = GetComponent<Animator>();
+	}
+	
+	void Update () {
+		Vector3 moveChar = new Vector3(CrossPlatformInputManager.GetAxis("Horizontal"),0,CrossPlatformInputManager.GetAxis("Vertical"));
+		transform.position += moveChar * Time.deltaTime * moveSpeed;
+		if (moveChar != Vector3.zero){
+			anim.SetBool("isWalking", true);
+		} else {
+			anim.SetBool("isWalking", false);
+		}
+	}
+}
